5eab46c9ef943781f2c9867c7aa63b0cb23733fe
mnsupreme <mnsupreme@gmail.com>
2019-03-27 22:10:51 -0700

    tcp split

--- tcp_stream.py
+++ tcp_stream.py
@@ -1,46 +1,37 @@
 from os import listdir
-import pyshark
+import sys
 from scapy.all import *
 
+os.chdir('lab/split')
+files =  os.listdir()
+streams = []
+
+def run(file):
+	scap = sniff(offline=file,filter='tcp and ip src net 192.168 and not ip dst net 192.168')
+
+	cap = sorted(cap, key=lambda pkt:int(pkt.tcp.stream))
+
+	global streams
 
 
-scap = sniff(offline='test_capture.pcap',filter='tcp and ip src net 192.168 and not ip dst net 192.168')
-cap = pyshark.FileCapture('test_capture.pcap',display_filter='ip.src == 192.168.0.0/16 and ip.dst != 192.168.0.0/16 and tcp',override_prefs={'tcp.relative_sequence_numbers':False},disable_protocol='UDP',keep_packets=True)
-
-max_number = 0
-
-streams[]
-cap = sorted(cap, key=lambda pkt:int(pkt.tcp.stream))
-
-
-
-for pkt in a:
-	match = {
-		'dst_ip': pkt.ip.dst,
-		'src_ip': pkt.ip.src,
-		'dst_port': int(pkt.tcp.dstport),
-		'src_port': int(pkt.tcp.srcport),
-		'id':int(pkt.ip.id,16),
-		'checksum':int(pkt.tcp.checksum,16),
-		'seq':int(pkt.tcp.seq)
-		'ack':int(pkt.tcp.ack)
-	}
-	scapy_packet =None
-	for scp in scap:
-		if scp['IP'].src = match['src_ip'] and scp['IP'].dst = match['dst_ip'] and src['IP'].id = match['id'] and src['TCP'].sport = match['src_port'] and src['TCP'].dport = match['dst_port'] and src['TCP'].seq = match['seq'] and src['TCP'].ack = match['ack'] and src['TCP'].chksum = match['checksum']:
-			scapy_packet = scp
-	if scapy_packet == None:
-		print("error: No matching packet")
-		break
-	else:
-		file = pkt.tcp.stream + '.pcap'
-		if int(pkt.tcp.stream) not in streams:
-			wrpcap(file,scapy_packet)
+		scapy_packet =None
+		for scp in scap:
+			if scp['IP'].src == match['src_ip'] and scp['IP'].dst == match['dst_ip'] and scp['IP'].id == match['id'] and scp['TCP'].sport == match['src_port'] and scp['TCP'].dport == match['dst_port'] and scp['TCP'].seq == match['seq'] and scp['TCP'].ack == match['ack'] and scp['TCP'].chksum == match['checksum']:
+				scapy_packet = scp
+		if scapy_packet == None:
+			print("error: No matching packet")
+			sys.exit(0)
 		else:
-			wrpcap(file,scapy_packet,append=True)
+			file = "../streams/" + pkt.tcp.stream + '.pcap'
+			if int(pkt.tcp.stream) not in streams:
+				streams.append(int(pkt.tcp.stream))
+				wrpcap(file,scapy_packet)
+			else:
+				wrpcap(file,scapy_packet,append=True)
 
 		
-
+for file in files:
+	run(file)
 			
 
 
---
+++ test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import os, subprocess, sys, logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+
+# Add some colouring for printing packets later
+YELLOW = '\033[93m'
+GREEN = '\033[92m'
+END = '\033[0m'
+
+if len(sys.argv) != 3:
+	print('Usage: ./pcap-streams.py pcapfile outputfolder')
+	print('Example: ./pcap-streams.py /tmp/test.pcap /tmp/output')
+	sys.exit(1)
+
+pcap = sys.argv[1]
+folder = sys.argv[2]
+
+tcp_stream_index = []
+tcp_stream_file = []
+
+devnull = open('/dev/null', 'w')
+
+# Check to see if the folder exists already
+if not os.path.exists(folder):
+	print(YELLOW + '[!] Folder doesnt exist so creating ' + folder + END)
+	os.makedirs(folder)
+
+# Function to extract TCP streams
+def tcp_stream(pcap):
+	print(GREEN + '[+] Extracting TCP streams' + END)
+	# Create a list of the tcp streams in the pcap file and save them as an index
+	cmd = 'tshark -r ' + pcap + ' -T fields -e tcp.stream'
+	p = os.popen(cmd).readlines()
+	for x in p:
+		if x not in tcp_stream_index:
+			tcp_stream_index.append(x)
+	# Now we are going to write out all the streams as a pcap file
+	try:
+		for y in tcp_stream_index:
+			y = y.strip('\n')
+			dumpfile = folder + '/tcp-stream' + y + '.pcap'
+			if 'tcp-stream.pcap' in dumpfile:
+				pass
+			else:
+				cmd = 'tshark -r ' + pcap + ' -Y "tcp.stream eq ' + y + '" -w ' + dumpfile
+				if dumpfile not in tcp_stream_file:
+					tcp_stream_file.append(dumpfile)
+				subprocess.run(cmd, shell=True, stdout=devnull, stderr=devnull)
+				#os.popen(cmd)
+				#print(cmd)
+	except:
+		pass
+
+	print(YELLOW + '[!] There are ' + str(len(tcp_stream_file)) + ' TCP streams saved in: ' + folder + END)
+
+
+tcp_stream(pcap)
