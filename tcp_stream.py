a3fc829c1f939c90323338b6bca7430fd58edb22
mnsupreme <mnsupreme@gmail.com>
2019-04-02 22:20:02 -0700

    working script

--- tcp_stream.py
+++ tcp_stream.py
@@ -1,37 +1,41 @@
 from os import listdir
 import sys
 from scapy.all import *
+from multiprocessing.pool import ThreadPool
 
-os.chdir('lab/split')
-files =  os.listdir()
-streams = []
+
 
 def run(file):
+	global streams
+	global write
+	global src
+	file = src + '/' + file
 	scap = sniff(offline=file,filter='tcp and ip src net 192.168 and not ip dst net 192.168')
 
-	cap = sorted(cap, key=lambda pkt:int(pkt.tcp.stream))
-
-	global streams
+	
 
 
-		scapy_packet =None
-		for scp in scap:
-			if scp['IP'].src == match['src_ip'] and scp['IP'].dst == match['dst_ip'] and scp['IP'].id == match['id'] and scp['TCP'].sport == match['src_port'] and scp['TCP'].dport == match['dst_port'] and scp['TCP'].seq == match['seq'] and scp['TCP'].ack == match['ack'] and scp['TCP'].chksum == match['checksum']:
-				scapy_packet = scp
-		if scapy_packet == None:
-			print("error: No matching packet")
-			sys.exit(0)
+	for pkt in scap:
+		socket = str(pkt['TCP'].sport) +'-'+ str(pkt['TCP'].dport) +'-'+ pkt['IP'].src + '-' + pkt['IP'].dst
+		dumpfile = write + '/' + socket + '.pcap'
+		if os.path.isfile(dumpfile):
+			wrpcap(dumpfile,pkt,append=True)
 		else:
-			file = "../streams/" + pkt.tcp.stream + '.pcap'
-			if int(pkt.tcp.stream) not in streams:
-				streams.append(int(pkt.tcp.stream))
-				wrpcap(file,scapy_packet)
-			else:
-				wrpcap(file,scapy_packet,append=True)
+			wrpcap(dumpfile,pkt)
+
+
 
 		
-for file in files:
-	run(file)
-			
+
+		
+if __name__ == '__main__':
+	src = sys.argv[1]
+	write = sys.argv[2]
+
+	#run(src)
+
+	files =  os.listdir(src)
+	pool = ThreadPool(6)
+	pool.map(run,files)
 
 
--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 import os, subprocess, sys, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+from multiprocessing.pool import ThreadPool
+import subprocess
 
 # Add some colouring for printing packets later
 YELLOW = '\033[93m'
@@ -18,7 +20,8 @@ pcap = sys.argv[1]
 folder = sys.argv[2]
 
 
-tcp_stream_index = []
+tcp_stream_index = {}
+commands = []
 tcp_stream_file = []
 
 devnull = open('/dev/null', 'w')
@@ -33,6 +36,10 @@ def sort_packets(file):
 	sort = sorted(scap, key= lambda pkt:pkt['TCP'].seq)
 	wrpcap(file,sort)
 
+def run(cmd):
+	print("execute")
+	subprocess.run(cmd, shell=True, stdout=devnull, stderr=devnull)
+
 # Function to extract TCP streams
 def tcp_stream(pcap):
 
@@ -40,30 +47,38 @@ def tcp_stream(pcap):
 	# Create a list of the tcp streams in the pcap file and save them as an index
 	cmd = 'tshark -r ' + pcap+ ' -T fields -e tcp.stream'
 	p = os.popen(cmd).readlines()
-	for x in p:
-		if x not in tcp_stream_index:
-			tcp_stream_index.append(x)
+	print("reading file")
+	for x in range(0,len(p)):
+		print(str(x) + " out of " + str(len(p)))
+		tcp_stream_index[p[x]]=0
 	# Now we are going to write out all the streams as a pcap file
-	try:
-		for y in tcp_stream_index:
-			y = y.strip('\n')
-			dumpfile = folder + '/tcp-stream' + y + '.pcap'
-			if 'tcp-stream.pcap' in dumpfile:
-				pass
-			else:
-				cmd = 'tshark -r ' + pcap + ' -Y "tcp.stream eq ' + y + '" -w ' + dumpfile 
-				if dumpfile not in tcp_stream_file:
-					tcp_stream_file.append(dumpfile)
+	print("streams number " + str(len(tcp_stream_index.keys())))
+	# try:
+	# 	for y in tcp_stream_index.keys():
+	# 		y = y.strip('\n')
+	# 		dumpfile = folder + '/tcp-stream' + y + '.pcap'
+	# 		if 'tcp-stream.pcap' in dumpfile:
+	# 			pass
+	# 		else:
+	# 			print("compiling commands")
+	# 			cmd = 'tshark -r ' + pcap + ' -Y "tcp.stream eq ' + y + '" -w ' + dumpfile 
+	# 			if dumpfile not in tcp_stream_file:
+	# 				tcp_stream_file.append(dumpfile)
+	# 			commands.append(cmd)
 				#subprocess.run(cmd, shell=True, stdout=devnull, stderr=devnull)
-				os.system(cmd)
+				#os.system(cmd)
 				#print(cmd)
-	except:
-		pass
+	# except:
+	# 	pass
+
+	print("running commands")
+	# for cmd, rc in ThreadPool(6).imap_unordered(run, commands):
+	# 	if rc != 0:
+	# 		print('{cmd} failed with exit status: {rc}'.format(**vars()))
 
 	print(YELLOW + '[!] There are ' + str(len(tcp_stream_file)) + ' TCP streams saved in: ' + folder + END)
 
-
-
-tcp_stream(pcap)
-
+if __name__ == '__main__':
+	tcp_stream(pcap)
+	print("streams number " + str(len(tcp_stream_index.keys())))
 
