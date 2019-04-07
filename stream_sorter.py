b47b03dac2cac9c5962e877c07063159aef64753
mnsupreme <mnsupreme@gmail.com>
2019-04-07 10:46:07 -0700

    new stream sorter

--- stream_sorter.py
+++ stream_sorter.py
@@ -8,28 +8,38 @@ from multiprocessing.pool import ThreadPool
 def sort(file):
 	global streams
 	global src
+	global counter
+	global files
+	global unsorted
+	counter += 1
+	print('file ' + str(counter) + ' out of ' + str(len(files)))
 	file = src + '/' + file
 	print(file)
-	scap = sniff(offline=file)
+	try:
+		scap = sniff(offline=file)
 
 	
-	scap =sorted(scap,key=lambda x:x['TCP'].seq)
-	wrpcap(file,scap)
-	print("freeing memory")
-	del scap
-	gc.collect()
-		
+		scap =sorted(scap,key=lambda x:x['TCP'].seq)
+		wrpcap(file,scap)
+		print("freeing memory")
+		del scap
+		gc.collect()
+	except:
+		unsorted.append(file)
+		print('problem sorting file ' + file)
 
 		
 if __name__ == '__main__':
 	src = sys.argv[1]
 	files =  os.listdir(src)
-
+	unsorted = []
 	# for file in files:
 	# 	run(file)
+	counter = 0
 
 	
 	pool = ThreadPool(6)
 	pool.map(sort,files)
 	pool.close()
 	pool.join()
+	print(unsorted)
